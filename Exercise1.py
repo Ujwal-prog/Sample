76f79da63ebe3acc40070852601358dda322e438
Ujwal-prog <ujjwaldh@gmail.com>
2024-06-28 10:15:34 -0400

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,32 @@
+'''x=1
+y=2.8
+z= 1j
+
+print(type(x))
+print(type(y))
+print(type(z))'''
+
+
+'''x = 1
+y = 354354635131351121
+z = -35435434
+D = 1.10
+E =35e3
+F =-87.7e100
+A =3+5j
+B =5j
+C =-5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(D))
+print(type(E))
+print(type(F))
+print(type(A))
+print(type(B))
+print(type(C))'''
+
+
+
+
